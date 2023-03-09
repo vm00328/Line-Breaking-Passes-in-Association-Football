035b4dc2ceba6f59988d62646ad127d95d45970e
Vladislav Manolov <vm00328@surrey.ac.uk>
2023-03-09 18:58:21 +0100

    clustering_v1&v2

---
+++ AZ/clustering_v2.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 17:59:48 2023
+
+@author: Vlado
+"""
+import matplotlib.pyplot as plt
+from mplsoccer import Pitch
+from matplotlib import animation
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from tqdm import tqdm
+import ffmpeg
+import matplotlib
+import os
+from matplotlib.animation import FuncAnimation, PillowWriter, FFMpegWriter, MovieWriter
+from matplotlib.text import Text
+
+df_AZ_VIT_cleaned = pd.read_csv("C:/Users/Vlado/Desktop/AZ/AZ/data/AZ_VIT_tracking_cleaned.csv")
+df_tracking_home = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team']==0]
+df_tracking_home = df_tracking_home[(df_tracking_home['frame'] >= 1690842) & (df_tracking_home['frame'] < 1690942)].copy()
+df_tracking_home_10 = df_tracking_home.drop(df_tracking_home[df_tracking_home['jersey_no'] == 16].index)
+df_tracking_ball = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team'].isna() & (df_AZ_VIT_cleaned['frame'] >=1690842) & (df_AZ_VIT_cleaned['frame'] < 1690942)].copy()
+
+pitch = Pitch(pitch_type='tracab', goal_type='line', pitch_length=105, pitch_width=68,
+              axis=True, label=True, corner_arcs=True, line_color='white', pitch_color = "grass", stripe =True)
+fig, ax = pitch.draw(figsize=(16, 10.4))
+
+marker_kwargs = {'marker': 'o', 'markeredgecolor': 'black', 'linestyle': 'None'}                 
+ball, = ax.plot([], [], ms=8, markerfacecolor='w', zorder=3, **marker_kwargs)
+away, = ax.plot([], [], ms=11, markerfacecolor='#0000FF', **marker_kwargs) #blue
+home, = ax.plot([], [], ms=11, markerfacecolor='#ff0000', **marker_kwargs) #red
+X = df_tracking_home_10[['x']].values
+kmeans = KMeans(n_clusters=3, random_state=0)
+kmeans.fit(X)
+
+# Add cluster labels to the dataframe
+df_tracking_home = df_tracking_home.assign(cluster=0)
+df_tracking_home_10['cluster'] = kmeans.labels_
+# Perform K-means clustering on x coordinates of the players for each team
+#kmeans_home = KMeans(n_clusters=3, random_state=0).fit(df_tracking_home.loc[df_tracking_home.frame == frame, ['x']])
+player_labels={}
+cluster_lines = [[], [], []] # one list for each cluster
+prev_line_update = 0
+player_lines={}
+def animate(i):
+    ball.set_data(df_tracking_ball.iloc[i, 5], df_tracking_ball.iloc[i, 6])
+    frame = df_tracking_ball.iloc[i, 2]
+    home_frame = df_tracking_home_10[df_tracking_home_10['frame'] == frame]
+    home.set_data(home_frame['x'], home_frame['y'])
+    
+    global prev_line_update
+    
+    """ WORKS
+    # Create lines connecting players in the same cluster
+    if i % 25 == 0:
+        for c in range(3):
+            cluster_frame = home_frame[home_frame['cluster'] == c]
+            if len(cluster_frame) > 1:
+                x = cluster_frame['x'].values
+                y = cluster_frame['y'].values
+            for line in cluster_lines[c]:
+                line.remove()
+            line = ax.plot(x, y, linewidth=2, color='white')
+            cluster_lines[c] = line
+            """
+    if i % 5 == 0:
+        for c in range(3):
+            cluster_frame = home_frame[home_frame['cluster'] == c]
+            if len(cluster_frame) > 1:
+                x = cluster_frame['x'].values
+                y = cluster_frame['y'].values
+                # If a line for this cluster already exists, update its coordinates
+                if c in player_lines:
+                    player_lines[c].set_data(x, y)
+                # Otherwise, create a new line for this cluster
+                else:
+                    line, = ax.plot(x, y, linewidth=2, color='white')
+                    player_lines[c] = line
+        # Store the current frame as the previous line update
+        prev_line_update = i
+    
+    # update previous line update frame number
+    if (frame - prev_line_update) % 25 == 0:
+        prev_line_update = frame
+    
+    # Create Text objects for each player's jersey number
+    for i, row in home_frame.iterrows(): 
+        x, y = row['x'], row['y']
+        jersey_no = int(row['jersey_no'])
+        # if the player already has a label, move it to their current position
+        if i in player_labels:
+            label = player_labels[i]
+            label.set_text(str(jersey_no))
+            label.xy = (x, y)
+        # otherwise, create a new label and add it to the plot
+        else:
+            label = ax.annotate(str(jersey_no), xy=(x, y),
+                                fontsize=11, color='white', ha='center', va='center')
+            player_labels[i] = label
+        
+    # Remove labels for players who are not in the current frame
+    for i in list(player_labels):
+        if i not in home_frame.index:
+            label = player_labels.pop(i)
+            label.remove()    
+    return ball, home
+
+anim = animation.FuncAnimation(fig, animate, frames=len(df_tracking_ball), interval=50, blit=True)
+f = r"C://Users/Vlado/Documents/GitHub/AZ/AZ/animations/proba18.gif" # try with .mov, .avi
+writervideo = animation.PillowWriter(fps = 25)
+anim.save(f, writer = writervideo)
--- AZ/pitch.py
+++ AZ/pitch.py
@@ -14,183 +14,88 @@ from sklearn.cluster import KMeans
 from tqdm import tqdm
 import imageio
 import ffmpeg
+import matplotlib
 import os
 from matplotlib.animation import FuncAnimation, PillowWriter, FFMpegWriter, MovieWriter
 from matplotlib.text import Text
+from scipy.spatial.distance import cdist
+from matplotlib.patches import FancyArrowPatch, ArrowStyle, FancyArrow
 
 df_AZ_VIT_cleaned = pd.read_csv("C:/Users/Vlado/Desktop/AZ/AZ/data/AZ_VIT_tracking_cleaned.csv")
 df_tracking_home = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team']==0]
 df_tracking_away = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team']==1]
 
-# Getting subset of the data (frame 1690841 is when the ball is first detected as alive)
 df_tracking_away = df_tracking_away[(df_tracking_away['frame'] >= 1690842) & (df_tracking_away['frame'] < 1690942)].copy()
 df_tracking_home = df_tracking_home[(df_tracking_home['frame'] >= 1690842) & (df_tracking_home['frame'] < 1690942)].copy()
 
-# Creating a dataframe to hold the ball data
 df_tracking_ball = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team'].isna() & (df_AZ_VIT_cleaned['frame'] >=1690842) & (df_AZ_VIT_cleaned['frame'] < 1690942)].copy()
 #df_tracking_ball.drop(['team', 'jersey_no'], axis = 1, inplace = True) #if inplace is False, the ball doesn't appear
 
-# Creating the pitch interface
 pitch = Pitch(pitch_type='tracab', goal_type='line', pitch_length=105, pitch_width=68,
               axis=True, label=True, corner_arcs=True, line_color='white', pitch_color = "grass", stripe =True)
 fig, ax = pitch.draw(figsize=(16, 10.4))
 
-# Setting up the pitch plot markers we want to animate
 marker_kwargs = {'marker': 'o', 'markeredgecolor': 'black', 'linestyle': 'None'}                 
 ball, = ax.plot([], [], ms=8, markerfacecolor='w', zorder=3, **marker_kwargs)
 away, = ax.plot([], [], ms=11, markerfacecolor='#0000FF', **marker_kwargs) #blue
 home, = ax.plot([], [], ms=11, markerfacecolor='#ff0000', **marker_kwargs) #red
-
-player_labels = {}
-player_arrows = {}
+player_labels={}
 def animate(i):
+    #global frame
     """ Function to animate the data. Each frame it sets the data for the players and the ball."""
-    # set the ball data with the x and y positions for the ith frame
-    ball.set_data(df_tracking_ball.iloc[i, 5], df_tracking_ball.iloc[i, 6]) # IMPORTANT, DEPENDS ON WHETHER COLUMNS WERE DROPPED
-    # get the frame id for the ith frame
-    frame = df_tracking_ball.iloc[i, 2]
+    ball.set_data(df_tracking_ball.iloc[i, 5], df_tracking_ball.iloc[i, 6])
+    
+    frame = df_tracking_ball.iloc[i, 2] # get the frame id for the ith frame
     # set the player data using the frame id .set_data takes takes two one-dimensional arrays represneting x and y values to plot
     away.set_data(df_tracking_away.loc[df_tracking_away.frame == frame, 'x'],
                   df_tracking_away.loc[df_tracking_away.frame == frame, 'y'])
     home.set_data(df_tracking_home.loc[df_tracking_home.frame == frame, 'x'],
                   df_tracking_home.loc[df_tracking_home.frame == frame, 'y'])
     
-    # create Text objects for each player's jersey number
-    for i, row in df_tracking_home[df_tracking_home['frame']==frame].iterrows(): #df_tracking_home['frame'].min()
-        # get the player's current position
+   # create Text objects for each player's jersey number
+    for i, row in df_tracking_home[df_tracking_home['frame']==frame].iterrows():
         x, y = row['x'], row['y']
+        vx, vy = row['vx'], row['vy']
+        
+        p = pitch.arrows(x, y, vx, vy, alpha=0.8, cmap='coolwarm_r',
+                          headaxislength=3, headlength=3, headwidth=4, width=2, ax=ax, zorder = 10)
         jersey_no = int(row['jersey_no'])
         # if the player already has a label, move it to their current position
         if i in player_labels:
             label = player_labels[i]
             label.set_text(str(jersey_no))
             label.xy = (x, y)
-            # otherwise, create a new label and add it to the plot
+        # otherwise, create a new label and add it to the plot
         else:
-            label = ax.annotate(str(jersey_no), xy=(x, y),
-                                    fontsize=11, color='white', ha='center', va='center')
+            label = ax.annotate(str(jersey_no), xy=(x, y), fontsize=11, color='white', ha='center', va='center')
             player_labels[i] = label
-
-        # create Arrow objects for each player's direction HOME TEAM
-        vx, vy = row['vx'], row['vy']
-        if i in player_arrows:
-            player_arrows[i].remove()
-        arrow = ax.arrow(x, y, vx, vy, head_width=1, head_length=1, fc='red', ec='red')
-        player_arrows[i] = arrow  
-
-    for i, row in df_tracking_away[df_tracking_away['frame']==frame].iterrows(): #df_tracking_away['frame'].min()
-        # get the player's current position
+    
+    for i, row in df_tracking_away[df_tracking_away['frame']==frame].iterrows():
+        # get the player's current position and velocity
         x, y = row['x'], row['y']
+        vx, vy = row['vx'], row['vy']
         jersey_no = int(row['jersey_no'])
         if i in player_labels:
-           label = player_labels[i]
-           label.set_text(str(jersey_no))
-           label.xy = (x, y)
+          label = player_labels[i]
+          label.set_text(str(jersey_no))
+          label.xy = (x, y)
         else:
-           label = ax.annotate(str(jersey_no), xy=(x, y),
-                                   fontsize=11, color='white', ha='center', va='center')
-           player_labels[i] = label
-           
-        # create Arrow objects for each player's direction AWAY TEAM
-        vx, vy = row['vx'], row['vy']
-        if i in player_arrows:
-            player_arrows[i].remove()
-        arrow = ax.arrow(x, y, vx, vy, head_width=1, head_length=1, fc='blue', ec='blue')
-        player_arrows[i] = arrow
-        
+          label = ax.annotate(str(jersey_no), xy=(x, y), fontsize=11, color='white', ha='center', va='center')
+          player_labels[i] = label
+   
     # remove labels for players who are not in the current frame
     for i in list(player_labels):
-        if i not in df_tracking_home[df_tracking_home['frame']==frame].index and i not in df_tracking_away[df_tracking_away['frame']==frame].index:
-            label = player_labels.pop(i)   #old approach: player_labels[i].remove()
-            label.remove()                 #old approach: del player_labels[i]
+       if i not in df_tracking_home[df_tracking_home['frame']==frame].index and i not in df_tracking_away[df_tracking_away['frame']==frame].index:
+           label = player_labels.pop(i)
+           label.remove()
     
     return ball, away, home
-
+    
 # frames=len(df_tracking_ball) needed only when exporting the animation; interval=50 is an optional argument and sets the interval between frames in miliseconds
 anim = animation.FuncAnimation(fig, animate, frames=len(df_tracking_ball), interval=50, blit=True)
-
-f = r"C://Users/Vlado/Documents/GitHub/AZ/AZ/animations/velocity_v2.gif" # try with .mov, .avi
+path = r"C://Users/Vlado/Documents/GitHub/AZ/AZ/animations"
+print(os.path.exists(path))
+os.makedirs(path, exist_ok=True)
+f = r"C://Users/Vlado/Documents/GitHub/AZ/AZ/animations/proba11.gif" # try with .mov, .avi
 writervideo = animation.PillowWriter(fps = 25)
 anim.save(f, writer = writervideo)
-
-
-
-
-
-
-
-
-
-
-################################################Clustering#############################################
-
-# Extract positional features from the tracking data
-features = df_tracking_home[["x", "y"]]
-
-# Computing centroids of each player at each frame
-centroids = df_AZ_VIT_cleaned.groupby(['frame', 'jersey_no'])[['x', 'y']].mean().reset_index()
-
-# Standardize the features
-scaler = StandardScaler()
-scaled_features = scaler.fit_transform(features)
-
-# Determine the optimal number of clusters using the elbow method
-wcss = []
-for i in tqdm(range(1, 11)):
-    #“init” argument is the method for initializing the centroid.
-    kmeans = KMeans(n_clusters = i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    kmeans.fit(centroids) #scaled_features replaced by centroids
-    wcss.append(kmeans.inertia_)
-    
-# Plot the elbow graph
-plt.plot(range(1, 11), wcss)
-plt.title('Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
-
-# Apply K-Means clustering with the optimal number of clusters based on the Elbow method
-clusters_number = 4
-kmeans = KMeans(n_clusters = clusters_number, init='k-means++', max_iter=300, n_init=10, random_state=0)
-cluster_labels = kmeans.fit_predict(features)
-print(cluster_labels)
-
-"""
-# Create a list of scatter plots, one for each frame
-scatter_plots = []
-for frame in range(1000):
-    # Get the centroids for the current frame
-    centroids_frame = centroids[centroids['frame'] == frame]
-    
-    # Create a scatter plot for the current frame
-    fig, ax = plt.subplots()
-    scatter = ax.scatter(centroids_frame['x'], centroids_frame['y'], cmap='Set1')
-    ax.set_xlim(0, 120)  # adjust limits as needed
-    ax.set_ylim(0, 80)
-    ax.set_title(f'Frame {frame}')
-    scatter_plots.append([scatter])
-"""
-    
-df_event_data = pd.read_csv('C:/Users/Vlado/Desktop/AZ/AZ/data/event_data_azvit.csv', sep = ';', header = None)
-df_head = df_AZ_VIT_cleaned.head(100)
-
-def sort_key(name):
-    # Extract the name and number from the string
-    name_parts = name.split('_')
-    name = name_parts[0]
-    number = int(name_parts[1].replace('.png', ''))
-    return name, number
-
-def create_animation(folder_path, output_path):
-    files = os.listdir(folder_path)
-    images = []
-    files = [name for name in files if not name.endswith('.npy')]
-    sorted_files = sorted(files, key = sort_key)
-    
-    for file_name in sorted_files:
-        if file_name.endswith('.png'):
-            file_path = os.path.join(folder_path, file_name)
-            images.append(imageio.imread(file_path))
-    fps = 5
-    imageio.mimsave(output_path, images, fps = fps)
-create_animation('folder_path', 'output_path_animation.mp4')
