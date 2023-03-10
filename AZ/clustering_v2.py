a346259559ea2d18105e8acb0136b9c46901713f
Vladislav Manolov <vm00328@surrey.ac.uk>
2023-03-10 10:58:04 +0100

    optimised code (shorter) + larger markers

--- AZ/clustering_v2.py
+++ AZ/clustering_v2.py
@@ -19,27 +19,25 @@ from matplotlib.text import Text
 
 df_AZ_VIT_cleaned = pd.read_csv("C:/Users/Vlado/Desktop/AZ/AZ/data/AZ_VIT_tracking_cleaned.csv")
 df_tracking_home = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team']==0]
-df_tracking_home = df_tracking_home[(df_tracking_home['frame'] >= 1690842) & (df_tracking_home['frame'] < 1690992)].copy()
+df_tracking_home = df_tracking_home[(df_tracking_home['frame'] >= 1690842) & (df_tracking_home['frame'] < 1690942)].copy()
 df_tracking_home_10 = df_tracking_home.drop(df_tracking_home[df_tracking_home['jersey_no'] == 16].index)
 df_tracking_away = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team']==1] #MISSING frame stamp but works?
-df_tracking_ball = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team'].isna() & (df_AZ_VIT_cleaned['frame'] >=1690842) & (df_AZ_VIT_cleaned['frame'] < 1690992)].copy()
+df_tracking_ball = df_AZ_VIT_cleaned[df_AZ_VIT_cleaned['team'].isna() & (df_AZ_VIT_cleaned['frame'] >=1690842) & (df_AZ_VIT_cleaned['frame'] < 1690942)].copy()
 
 pitch = Pitch(pitch_type='tracab', goal_type='line', pitch_length=105, pitch_width=68,
               axis=True, label=True, corner_arcs=True, line_color='white', pitch_color = "grass", stripe =True)
 fig, ax = pitch.draw(figsize=(16, 10.4))
 
 marker_kwargs = {'marker': 'o', 'markeredgecolor': 'black', 'linestyle': 'None'}                 
-ball, = ax.plot([], [], ms=8, markerfacecolor='w', zorder=3, **marker_kwargs)
-away, = ax.plot([], [], ms=11, markerfacecolor='#0000FF', **marker_kwargs) #blue
-home, = ax.plot([], [], ms=11, markerfacecolor='#ff0000', **marker_kwargs) #red
+ball, = ax.plot([], [], ms = 11, markerfacecolor='w', zorder=3, **marker_kwargs)
+away, = ax.plot([], [], ms = 15, markerfacecolor='#0000FF', **marker_kwargs) #blue
+home, = ax.plot([], [], ms = 15, markerfacecolor='#ff0000', **marker_kwargs) #red
 X = df_tracking_home_10[['x']].values
-kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+kmeans = KMeans(n_clusters=3, random_state=0).fit(X) # Perform K-means clustering on x coordinates of the players for each team
 
 # Add cluster labels to the dataframe
 df_tracking_home = df_tracking_home.assign(cluster=0)
 df_tracking_home_10['cluster'] = kmeans.labels_
-# Perform K-means clustering on x coordinates of the players for each team
-#kmeans_home = KMeans(n_clusters=3, random_state=0).fit(df_tracking_home.loc[df_tracking_home.frame == frame, ['x']])
 player_labels={}
 prev_line_update = 0
 player_lines={}
@@ -63,17 +61,14 @@ def animate(i):
                 y = cluster_frame['y'].values
                 # If a line for this cluster already exists, update its coordinates
                 if c in player_lines:
-                    player_lines[c].set_data(x, y)
+                    prev_line = player_lines[c]
+                    prev_line.set_data(x, y)
                 # Otherwise, create a new line for this cluster
                 else:
                     line, = ax.plot(x, y, linewidth=2, color='white')
                     player_lines[c] = line
         # Store the current frame as the previous line update
         prev_line_update = i
-    
-    # update previous line update frame number
-    if (frame - prev_line_update) % 25 == 0:
-        prev_line_update = frame
     
     # Create Text objects for each player's jersey number
     for i, row in df_tracking_home[df_tracking_home['frame']==frame].iterrows(): 
@@ -109,6 +104,6 @@ def animate(i):
     return ball, away, home
 
 anim = animation.FuncAnimation(fig, animate, frames=len(df_tracking_ball), interval=50, blit=True)
-f = r"C://Users/Vlado/Documents/GitHub/AZ/AZ/animations/proba34.gif"
+f = r"C://Users/Vlado/Documents/GitHub/AZ/AZ/animations/proba41.gif"
 writervideo = animation.PillowWriter(fps = 25)
 anim.save(f, writer = writervideo)
